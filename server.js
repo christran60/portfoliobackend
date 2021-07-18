b9ba961b8d44a7c287f6a421e2a5d5f2077c5bf1
Chris <ctran209@outlook.com>
2021-07-18 11:20:50 -0700

    backend fixing

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ app.use(express.json());
 app.use("/", router);
 app.listen(port, () => console.log("Server Running"));
 console.log('Our app is running on http://localhost:' + port);
-console.log(__dirname);
-if (process.env.NODE_ENV === "production"){
-app.use(express.static(__dirname + '/'));
-}
+// console.log(__dirname);
 // if (process.env.NODE_ENV === "production"){
-//   app.use(express.static("build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname,  "build"));
-//   });
+// app.use(express.static(__dirname + '/'));
 // }
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(__dirname));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname));
+  });
+}
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {

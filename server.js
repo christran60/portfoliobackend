ec31f0d26932dd2df7f080dce4f369458caf4f2e
Christopher Tran <65186360+christran60@users.noreply.github.com>
2021-07-17 21:25:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("https://christophertran.netlify.app/", router);
+app.use("/ContactPage", router);
 app.listen(port, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({

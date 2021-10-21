13baf18da81bfc3231b7c415180d47cc08b18161
Chris <ctran209@outlook.com>
2021-10-21 15:32:52 -0700

    updated error msg

--- server.js
+++ server.js
@@ -16,16 +16,6 @@ app.use(express.json());
 app.use("/", router);
 app.listen(port, () => console.log("Server Running"));
 console.log('Our app is running on http://localhost:' + port);
-// console.log(__dirname);
-// if (process.env.NODE_ENV === "production"){
-// app.use(express.static(__dirname + '/'));
-// }
-// if (process.env.NODE_ENV === "production"){
-//   app.use(express.static(__dirname));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname));
-//   });
-// }
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -42,8 +32,6 @@ const contactEmail = nodemailer.createTransport({
     }
   });
   router.post("/", (req, res) => {
-  // router.post("https://christophertran.netlify.app/ContactPage/", (req, res) => {
-    // router.post("https://myportfoliocontactform.herokuapp.com/", (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const message = req.body.message; 
@@ -57,7 +45,7 @@ const contactEmail = nodemailer.createTransport({
     };
     contactEmail.sendMail(mail, (error) => {
       if (error) {
-        res.json({ status: "ERROR" });
+        res.json({ status: "ERROR, go ahead and email me directly instead! My email is ctran209@outlook.com." });
       } else {
         res.json({ status: "Message Sent!" });
 
